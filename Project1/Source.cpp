2647a1072da210ee0b0c7e3eb75d9c4787d245b7
Elshadai Veloso <150169442+Elshadaiv@users.noreply.github.com>
2024-02-27 20:55:03 +0000

    Update Source.cpp
    the push system on my visual studios isn't updating the code ill try get a fix to it soon

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -7,13 +7,16 @@
 #include <stdlib.h>
 #include <cstddef>
 
-#define SIZE 5
-
-struct Data 
+#struct Car
 {
-	char cars;
+	char Registration[20];
+	char make_model[20];
+	char colour[15];
+	int previous_owners;
+	bool reserved;
+	double reserveAmount;
+	float sale; //Special FEATURE
 };
-
 
 struct LinearNode 
 {
@@ -23,17 +26,106 @@ struct LinearNode
 
 struct LinearNode* front = NULL;
 struct LinearNode* last = NULL;
-
  
 
+void DisplayMenu()
+{
+	int option = 0;
 
+	while (true)
+	{
+		printf("1) Add a new car to the showroom\n");
+		printf("2) Sell a car from the showroom\n");
+		printf("3) Reserve a car in the showroom\n");
+		printf("4)  Unreserve a car in the showroom\n");
+		printf("5) View all cars in the showroom\n");
+		printf("6) View a specific car in the showroom\n");
+		printf("7) ShowRoom Sale\n"); //Special Feature
+		printf("8) Exit\n");
+		printf("Which will you like to choose: ");
+
+		scanf("%d", &option);
+
+		switch (choice)
+		{
+		case 1:
+			addCar();
+			break;
+
+		case 2:
+			sellCar();
+			break;
+
+		case 3:
+			reserveCar();
+			break;
+
+		case 4:
+			unreserveCar();
+			break;
+
+		case 5:
+			viewAllCars();
+			break;
+
+		case 6:
+			viewSpecificCar();
+			break;
+
+		case 7:
+			SP();
+			break;
+
+		case 8:
+			exit();
+			break;
+		}
+		
+	}
+}
 
 void main()
 {
+	displayMenu();
+	getchar;
+}
 
+// calling all my methods which are needed for thr question
+void addCar() 
+{
 
 }
 
+void sellCar()
+{
 
+}
+
+void reserveCar()
+{
+
+}
+
+void unreserveCar()
+{
+
+}
+
+void viewAllCars()
+{
+
+}
+
+void viewSpecificCar()
+{
+
+}
+
+void exit()
+{
+		printf("Thank you!");
+		return 0;
+}
+	
 
 	
