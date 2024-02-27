e5b1d55fcf9a717f79634a347ef66a99261dabdb
Elsha <Elsha@ELSHADAI>
2024-02-27 19:11:06 +0000

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,39 @@
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#define bool int
+#define false 0
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstddef>
+
+#define SIZE 5
+
+struct Data 
+{
+	char cars;
+};
+
+
+struct LinearNode 
+{
+	struct Data *element;
+	struct LinearNode *next;
+};
+
+struct LinearNode* front = NULL;
+struct LinearNode* last = NULL;
+
+ 
+
+
+
+void main()
+{
+
+
+}
+
+
+
+	
